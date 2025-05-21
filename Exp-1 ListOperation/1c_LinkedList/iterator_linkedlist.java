a38bfd6c058ccd689548a37b8a92c180fa5fe8f0
Delta K17 <158074959+DeltaK17@users.noreply.github.com>
2025-05-21 21:53:21 +0530

    Update iterator_linkedlist.java

--- Exp-1 ListOperation/1c_LinkedList/iterator_linkedlist.java
+++ Exp-1 ListOperation/1c_LinkedList/iterator_linkedlist.java
@@ -1,3 +1,8 @@
+/*
+1. Write a Java program to iterate through all elements in a linked list starting at the
+specified position ( 2nd) using iterator ( hint : Iterator iteratorobj = listobj.listIterator(1))
+*/
+
 package Lab1;
 import java.util.*;
 
