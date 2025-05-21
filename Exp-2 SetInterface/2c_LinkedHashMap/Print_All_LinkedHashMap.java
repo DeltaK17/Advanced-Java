2d2af08233ac4790b998179eb0932c6f2f7d1f5c
Delta K17 <158074959+DeltaK17@users.noreply.github.com>
2025-05-21 22:10:24 +0530

    Update Print_All_LinkedHashMap.java

--- Exp-2 SetInterface/2c_LinkedHashMap/Print_All_LinkedHashMap.java
+++ Exp-2 SetInterface/2c_LinkedHashMap/Print_All_LinkedHashMap.java
@@ -1,3 +1,6 @@
+/*
+3.Include the following elements into the LinkedHashMap called programminglanguages and print all of them. ("Java", 1995);("Python", 1991);(“JavaScript", 1995);("C++", 1985); 
+*/
 package Lab2;
 import java.util.*;
 public class Print_All_LinkedHashMap {
