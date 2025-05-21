f60f9fc4aa56edf653c989a541c49033b450f4b7
Delta K17 <158074959+DeltaK17@users.noreply.github.com>
2025-05-21 21:49:57 +0530

    Update contains_red.java

--- Exp-1 ListOperation/1b_ArrayList/contains_red.java
+++ Exp-1 ListOperation/1b_ArrayList/contains_red.java
@@ -1,3 +1,8 @@
+/*
+1. Write a java program for getting different colors through ArrayList interface and search whether
+the color "Red" is available or not
+*/
+
 package Lab1;
 
 import java.util.*;
