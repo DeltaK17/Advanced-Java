b541851cb971c37037802ff04884d6d9dcebbc7d
Delta K17 <158074959+DeltaK17@users.noreply.github.com>
2025-05-21 08:41:25 -0700

    Add files via upload

---
+++ Exp-3 StringOperation/3c_String_Exercises/Reads_The_Same_Backward_as_Forward.java
@@ -0,0 +1,13 @@
+package Lab3;
+
+public class Reads_The_Same_Backward_as_Forward {
+	 public static boolean isPalindrome(String str) {
+	        String cleaned = str.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+	        return cleaned.equals(new StringBuilder(cleaned).reverse().toString());
+	    }
+
+	    public static void main(String[] args) {
+	        String input = "A man, a plan, a canal: Panama";
+	        System.out.println("Is palindrome: " + isPalindrome(input));
+	    }
+}
